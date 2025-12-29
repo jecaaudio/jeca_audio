32ca667aac7e1e5b71f69019fb7c0e7febc4cf18
jecaaudio <eljecatv@gmail.com>
2025-12-28 23:14:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,30 @@ const infoEmpresa = {
     // --- SONIDO / SOUND ---
     {
         id: "zlx12bt",
-        categoria: "speakers",
-        nombre: "Electro-Voice ZLX-12BT",
-        descripcion: "Altavoz activo de 12\" con Bluetooth. Perfecto para eventos medianos y monitoreo profesional.",
-        fotos: ["img/productos/audio/ZLX12BT/1.jpg"]
+            categoria: "speakers", // <--- Añade esto aquí
+            nombre: "Electro-Voice ZLX-12BT", 
+            descripcion: "12-inch 1000W Bluetooth Powered Loudspeaker. Pro-grade audio quality.",
+            fotos: [
+                "img/productos/audio/zlx12bt/zlx12bt.jpg",
+                "img/productos/audio/zlx12bt/zlx12bt2.jpg",
+                "img/productos/audio/zlx12bt/zlx12bt1.jpg",
+                "img/productos/audio/zlx12bt/zlx12bt3.jpg",
+                "img/productos/audio/zlx12bt/zlx12bt4.jpg",
+                "img/productos/audio/zlx12bt/zlx12btzlx12bt5.jpg",
     },
     {
-        id: "elx200",
-        categoria: "subs",
-        nombre: "Electro-Voice ELX200-18SP",
-        descripcion: "Subwoofer activo de 18\". Potencia de bajos profunda para llenar cualquier pista de baile.",
-        fotos: ["img/productos/audio/ELX200/1.jpg"]
+       id: "elx200",
+            categoria: "subs", // <--- Añade esto aquí
+            nombre: "Electro-Voice ELX200-18SP", 
+            descripcion: "Professional 18-inch powered subwoofer. High-impact sound for any event.",
+            fotos: [
+                "img/productos/audio/zlx200/zlx200.jpg",
+                "img/productos/audio/zlx200/zlx200 1.jpg",
+                "img/productos/audio/zlx200/zlx200 2.jpg",
+                "img/productos/audio/zlx200/zle200 4.jpg",
+                "img/productos/audio/zlx200/zlx200 3.jpg",
+                "img/productos/audio/zlx200/zlx 2005.jpg",
+
     },
     {
         id: "ui24r",
